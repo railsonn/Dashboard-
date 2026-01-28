03f67eae5b81b421073305aa7b755738103c8e39
railsonn <railsongamer99@gmail.com>
2026-01-28 15:52:16 -0300

    migration change name references

---
+++ db/migrate/20260128185153_fix_category_reference_in_budgets.rb
@@ -0,0 +1,4 @@
+class FixCategoryReferenceInBudgets < ActiveRecord::Migration[8.0]
+  def change
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,37 +12,48 @@ require 'faker'
 
 puts "Criando accounts..."
 
-User.create!(
-  email: 'user@example.com',
-  name: 'Usuário Exemplo')
+# User.create!(
+#   email: 'user@example.com',
+#   name: 'Usuário Exemplo')
 
-30.times do
-  Account.create!(
-    name: Faker::Bank.name,
-    balance: Faker::Number.decimal(l_digits: 4, r_digits: 2),
-    accountType: ['Corrente', 'Poupança', 'Investimento'].sample,
-    user: User.first
-  )
-end
+# 30.times do
+#   Account.create!(
+#     name: Faker::Bank.name,
+#     balance: Faker::Number.decimal(l_digits: 4, r_digits: 2),
+#     accountType: ['Corrente', 'Poupança', 'Investimento'].sample,
+#     user: User.first
+#   )
+# end
 
-30.times do 
-  Transaction.create!(
-    transaction_type: ['DEBIT', 'CREDIT'].sample,
-    amount: Faker::Number.decimal(l_digits: 3, r_digits: 2),
-    description: [
-      "Compra no cartão de crédito",
-      "Transferência recebida",
-      "Transferência enviada",
-      "Pix recebido",
-      "Pix enviado",
-      "Saque em caixa eletrônico",
-      "Depósito em dinheiro",
-      "Pagamento de boleto",
-      "Assinatura mensal"
-    ].sample,
-    transaction_date: Faker::Date.backward(days: 365),
+# 30.times do 
+#   Transaction.create!(
+#     transaction_type: ['DEBIT', 'CREDIT'].sample,
+#     amount: Faker::Number.decimal(l_digits: 3, r_digits: 2),
+#     description: [
+#       "Compra no cartão de crédito",
+#       "Transferência recebida",
+#       "Transferência enviada",
+#       "Pix recebido",
+#       "Pix enviado",
+#       "Saque em caixa eletrônico",
+#       "Depósito em dinheiro",
+#       "Pagamento de boleto",
+#       "Assinatura mensal"
+#     ].sample,
+#     transaction_date: Faker::Date.backward(days: 365),
+#     user: User.all.sample,
+#     account: Account.all.sample
+#   )
+# end
+
+
+5.times do
+  Budget.create!(
+    limit_amount: Faker::Number.decimal(l_digits: 4, r_digits: 2),
+    month: Faker::Date.backward(days: 365).strftime("%B"),
+    year: Faker::Date.backward(days: 365).strftime("%Y"),
     user: User.all.sample,
-    account: Account.all.sample
+    categories_id: Category.first
   )
 end
-puts "Transactions, accounts e user criadas com sucesso!"
+puts "Budgets criadas com sucesso!"
