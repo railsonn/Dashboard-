50a92c8174c4fa9bef2eb0d066577b2b3aee9aea
railsonn <railsongamer99@gmail.com>
2026-01-29 14:12:21 -0300

    finished adding helper of the style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def nav_active(path)
     current_page?(path) ? "active" : ""
   end
+
+  def background_tr(index)
+    if index.even?
+      "background: #0d1b2a;
+       color: white;"
+    else 
+      "background: #161B2E;
+       color: white;"
+    end
+  end
 end
