2ef9a578ae5c7943217e0e714a0ad4c80844dc4d
railsonn <railsongamer99@gmail.com>
2026-01-29 12:04:18 -0300

    create helper for stylized of the table

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,2 +1,11 @@
 module AccountsHelper
+  def background_tr(index)
+    if index.even?
+      "background: #0d1b2a;
+       color: white;"
+    else 
+      "background: #161B2E;
+       color: white;"
+    end
+  end
 end
