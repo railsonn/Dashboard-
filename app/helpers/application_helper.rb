7f46e7fac25815d53e99fe193c69c69ac079a49b
railsonn <railsongamer99@gmail.com>
2026-01-30 23:17:15 -0300

    resolved conflict themes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def nav_active(path)
     current_page?(path) ? "active" : ""
   end
+
+  def check(theme)
+    puts "#{theme} =================================================="
+  end
 end
