bfe485a20b6e0f43aa00d029b70e980b17ef731e
railsonn <railsongamer99@gmail.com>
2026-02-03 21:11:14 -0300

    implemented i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,22 @@
 class ApplicationController < ActionController::Base
   before_action :set_theme
+  before_action :set_locale
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+
+  def set_locale
+    I18n.locale =
+      if params[:locale].present?
+        params[:locale]
+      else
+        I18n.default_locale
+      end
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 
 
   private 
