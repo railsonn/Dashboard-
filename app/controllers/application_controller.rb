6fea7fcbb3472722a73ac22928b43815f5728b49
railsonn <railsongamer99@gmail.com>
2026-02-03 21:42:16 -0300

    configuration links for implementtation of I18N

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,7 @@ class ApplicationController < ActionController::Base
 
 
   def set_locale
-    I18n.locale =
-      if params[:locale].present?
-        params[:locale]
-      else
-        I18n.default_locale
-      end
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 
   def default_url_options
